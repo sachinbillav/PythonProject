3a9e5a25408b9bc7fc4e38d4fd5bd06f82f19a6e
Sachin <billavsachin5@gmail.com>
2024-07-23 19:02:52 +0530

    added threading

--- multithreading.py
+++ multithreading.py
@@ -1,15 +1,26 @@
-# class Hello:
-#     def run(self):
-#         print("hello")
-#
-# class Hi:
-#     def run(self):
-#         print("Hi")
-#
-# t1 = Hello()
-# t2 = Hi()
-#
-# t1.run()
-# t2.run()
+from threading import *
+class Hello(Thread):
 
-print("dsds")
+    def run2(self):
+        for i in range(500):
+            print("hello")
+    def run(self):
+        self.run2()
+
+class Hi(Thread):
+    def run1(self):
+        for i in range(500):
+            print("Hi")
+    def run(self):
+        self.run1()
+
+
+
+t1 = Hello()
+t2 = Hi()
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print("#############################")
