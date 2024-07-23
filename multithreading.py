f7258b9fb4e29880168e28d83409d522c58cec09
Sachin <billavsachin5@gmail.com>
2024-07-23 18:25:32 +0530

    updated threading

--- multithreading.py
+++ multithreading.py
@@ -1,13 +1,15 @@
-class Hello:
-    def run(self):
-        print("hello")
+# class Hello:
+#     def run(self):
+#         print("hello")
+#
+# class Hi:
+#     def run(self):
+#         print("Hi")
+#
+# t1 = Hello()
+# t2 = Hi()
+#
+# t1.run()
+# t2.run()
 
-class Hi:
-    def run(self):
-        print("Hi")
-
-t1 = Hello()
-t2 = Hi()
-
-t1.run()
-t2.run()
+print("dsds")
